bd0120724ad6dc7705e0ce4e7c9d221684a70cb5
AndySmithQA <andy.smith@qa.com>
2023-02-22 13:00:17 +0000

    Final

--- src/components/items.jsx
+++ src/components/items.jsx
@@ -1,11 +1,11 @@
-import React from "react";
 
-function ItemCard({ symbol, name, price }) {
+
+function ItemCard({ symbol, name, price}) {
     return (
         <div className="item-card">
             <div className="symbol">{symbol}</div>
             <h3>{name}</h3>
-            <p>£{price.toFixed(2)}</p>
+            <p>{price}</p>
         </div>
     )
 }
